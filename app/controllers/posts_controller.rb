1597be6173fa7814e3d1a52fb59db98b02657b5a
heboha0504 <hebonobank@gmail.com>
2020-10-05 15:15:53 +0900

    AjaxAppの準備

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,9 +3,9 @@ class PostsController < ApplicationController
    @posts = Post.all #全てのレコードを＠postsに代入
  end
 
- def new
- end
-
  def create
   post.create(content: params[:content])
+  redirect_to action: :index
+ end
+  
 end
